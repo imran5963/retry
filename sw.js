f65d39641ab7e494f02b871cdbc947eac8d80e0d
IMRAN KHAN <127175518+imran5963@users.noreply.github.com>
2024-11-23 10:43:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('push', ()=>{
-    self.registration.sendNotification('test-message', {});
+self.addEventListener('push', function(e) {
+    const options = {
+        body: 'This notification was generated from push!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore',
+                title: 'Explore the world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: 'images/xmark.png'
+            }
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Hello world!', options)
+    );
 });
