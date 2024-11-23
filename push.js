7b319dd10cb533817174f05a7395145b7b3c6580
Imran <imrankhan59631@gmail.com>
2024-11-23 10:00:20 +0530

    initials

--- push.js
+++ push.js
@@ -6,8 +6,8 @@ var push =require('web-push')
 
 
 let vapidKeys={
-    publicKey:'BL9Mcd7Tu6RJKJjndBcw8vP1BoTPq7KBuSBqvA24xtqEQdlhzGy4RRoaCNPj-B13tjr0xpPAKHUKhv-bQ17bsJc',
-    privateKey:'-LGMsYyBVBVuRUC75vSFHPSJSj1qMHtGyjvkrq1j4rM'
+    publicKey:'BMeeoaZLn3kdro5ba21vzTM6NnQ33JZG1jTjXRN8pUacDDsXT9U0qu5gxUcfW9Kf0oY5VmJsRJ6ZGNA5Xvq-e4Y',
+    privateKey:'0RjfytzrFe0G8RUCms_PyjiAKZRqOr0sYfivmxtybV0'
 }
 
 push.setVapidDetails('mailto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey)
